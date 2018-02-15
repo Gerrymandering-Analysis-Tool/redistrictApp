5114290e7ca8299e42de8d4548605a39a42d9017
Ali Hamdany <hamdanyaztech@gmail.com>
2018-02-15 10:51:20 -0500

    adding new repo

---
+++ js/styler.js
@@ -0,0 +1,43 @@
+/**
+ * Applies a gradient style based on the 'census_variable' column.
+ * This is the callback passed to data.setStyle() and is called for each row in
+ * the data set.  Check out the docs for Data.StylingFunction.
+ *
+ * @param {google.maps.Data.Feature} feature
+ */
+function styleFeature(feature) {
+  var low = [5, 69, 54]; // color of smallest datum
+  var high = [151, 83, 34]; // color of largest datum
+
+  // delta represents where the value sits between the min and max
+  var delta = (feature.getProperty('census_variable') - censusMin) /
+    (censusMax - censusMin);
+
+  var color = [];
+  for (var i = 0; i < 3; i++) {
+    // calculate an integer color based on the delta
+    color[i] = (high[i] - low[i]) * delta + low[i];
+  }
+
+  // determine whether to show this shape or not
+  var showRow = true;
+  if (feature.getProperty('census_variable') == null ||
+    isNaN(feature.getProperty('census_variable'))) {
+    showRow = false;
+  }
+
+  var outlineWeight = 0.5,
+    zIndex = 1;
+  if (feature.getProperty('state') === 'hover') {
+    outlineWeight = zIndex = 2;
+  }
+
+  return {
+    strokeWeight: outlineWeight,
+    strokeColor: '#fff',
+    zIndex: zIndex,
+    fillColor: 'hsl(' + color[0] + ',' + color[1] + '%,' + color[2] + '%)',
+    fillOpacity: 0.75,
+    visible: showRow
+  };
+}
